39902ba550aaec9c336f26129f40dd6273a8162b
Nour Hamdy <nenohamdy32@gmail.com>
2022-07-25 19:52:56 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+// let firstName = document.getElementById("firstName")
+// let lastName = document.getElementById("lastName")
+// let email = document.getElementById("email")
+// let password = document.getElementById("password")
+// let formBtn = document.getElementById("formBtn")
+// let freeTrailForm = document.getElementById('free-trail-form')
+
+// const validateEmail = (email) => {
+//     return String(email)
+//       .toLowerCase()
+//       .match(
+//         /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+//       );
+//   };
+
+// freeTrailForm.addEventListener('submit',(e)=>{
+//     e.preventDefault()
+//     if(!validateEmail(email.value)){
+//         email.style.backgroundImage('"./images/icon-error.svg"')
+//         email.style.backgroundSize('20px')
+//         email.style.backgroundPosition("95% 15px")
+//       }
+// })
+
+ 
